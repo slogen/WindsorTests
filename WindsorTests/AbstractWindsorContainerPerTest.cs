7fc063be3a7dea9ff8de7ed65431a9d88278ddba
Helge Jensen <hej@scadaminds.com>
2017-10-20 13:23:31 +0200

    Tests showing how .Forward works

---
+++ WindsorTests/AbstractWindsorContainerPerTest.cs
@@ -0,0 +1,27 @@
+﻿using Castle.Windsor;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindsorTests
+{
+    public abstract class AbstractWindsorContainerPerTest
+    {
+        protected abstract IWindsorContainer CreateWindsorContainer();
+        private IWindsorContainer _windsorContainer;
+        protected IWindsorContainer WindsorContainer
+        {
+            get
+            {
+                return _windsorContainer ?? (_windsorContainer = CreateWindsorContainer());
+            }
+        }
+        [SetUp]
+        public void SetUp() { _windsorContainer = null; }
+        [TearDown]
+        public void TearDown() { _windsorContainer?.Dispose(); }
+    }
+}
---
+++ WindsorTests/ForwardTypeAndInterfaceRegistrations/ForwardBetweenTypeAndInterfaceRegistrationsTests.cs
@@ -0,0 +1,90 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindsorTests.ForwardTypeAndInterfaceRegistrations
+{
+    public class ForwardBetweenTypeAndInterfaceRegistrationsTests: AbstractWindsorContainerPerTest
+    {
+        public interface IFoo
+        {
+            int Id { get; }
+        }
+        public class Foo : IFoo
+        {
+            public Foo(int id)
+            {
+                Id = id;
+            }
+
+            public int Id { get; }
+            public override bool Equals(object obj)
+            {
+                return (obj is Foo) &&
+                    ((Foo)obj).Id == Id;
+            }
+            public override int GetHashCode() => Id;
+        }
+
+        public class Bar : IFoo
+        {
+            public Bar(IFoo other)
+            {
+                Other = other;
+            }
+
+            public int Id => Other.Id;
+
+            public IFoo Other { get; }
+            public override bool Equals(object obj)
+            {
+                return (obj is Bar) &&
+                    Other.Equals(((Bar)obj).Other);
+            }
+            public override int GetHashCode() => Id;
+        }
+        protected override IWindsorContainer CreateWindsorContainer()
+        {
+            var c = new WindsorContainer();
+            c.Register(
+            Component.For<Foo>().LifestyleTransient()
+                .DependsOn(Dependency.OnValue("id", -1))
+                .Named("Foo").Forward<IFoo>(),
+            Component.For<Bar>().LifestyleTransient()
+                .Named("Bar").Forward<IFoo>()
+                );
+            return c;
+        }
+        [Test]
+        public void UnnamedResolveReturnsFirstRegistration()
+        {
+            WindsorContainer.Resolve<IFoo>()
+                .Should()
+                .BeOfType<Foo>()
+                .And.Be(new Foo(-1), because: "We should get the Fpo registration");
+        }
+        [Test]
+        public void NamedFooResolveReturnsFoo()
+        {
+            WindsorContainer.Resolve<IFoo>("Foo")
+                .Should()
+                .BeOfType<Foo>()
+                .And.Be(new Foo(-1), because: "We should get the Foo registration");
+        }
+        [Test]
+        public void NamedBarResolveReturnsBar()
+        {
+            WindsorContainer.Resolve<IFoo>("Bar")
+                .Should()
+                .BeOfType<Bar>()
+                .And.Be(new Bar(new Foo(-1)), because: "We should get Bar registration, and then resolve the inner registration through the default");
+        }
+    }
+}
