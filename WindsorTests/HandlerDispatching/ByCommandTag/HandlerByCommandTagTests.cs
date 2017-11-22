d9f4bc096bd7bb8dfdaddf33062c99631652e679
Helge Jensen <hej@scadaminds.com>
2017-11-22 09:35:01 +0100

    Simple tests for command dispatching by type and tag

---
+++ WindsorTests/HandlerDispatching/ByCommandTag/HandlerByCommandTagTests.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WindsorTests.HandlerDispatching.ByCommandTag
+{
+
+
+    public class HandlerByCommandTagTests : AbstractWindsorContainerPerTest
+    {
+        public interface ICommand
+        {
+            int CommandTag { get; }
+        }
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+        public class CommandHandlerAttribute : Attribute
+        {
+            public int CommandTag { get; }
+
+            public CommandHandlerAttribute(int commandTag)
+            {
+                CommandTag = commandTag;
+            }
+        }
+
+        public interface IHandler
+        {
+            Task Execute(CancellationToken cancellationToken);
+        }
+
+        public interface IHandlerFactory
+        {
+            IHandler ForCommand(ICommand command);
+        }
+
+        public class HandlerFactorySelector : DefaultTypedFactoryComponentSelector
+        {
+            /// TODO: Remove this hacky way of registering
+            private readonly ConcurrentDictionary<int, Type> _registeredHandlers = new ConcurrentDictionary<int, Type>();
+            public HandlerFactorySelector(
+                bool getMethodsResolveByName = true,
+                bool fallbackToResolveByTypeIfNameNotFound = true)
+                : base(getMethodsResolveByName, fallbackToResolveByTypeIfNameNotFound)
+            {
+            }
+
+            public void Register(int commandType, Type handlerType)
+            {
+                _registeredHandlers[commandType] = handlerType;
+            }
+
+            protected override Type GetComponentType(MethodInfo method, object[] arguments)
+            {
+                var command = (ICommand)arguments[0];
+                var commandType = command.CommandTag;
+                Type t;
+                return !_registeredHandlers.TryGetValue(commandType, out t) ? null : t;
+            }
+        }
+
+        public class HandlerBase : IHandler
+        {
+            public ICommand Command { get; }
+            public HandlerBase(ICommand command)
+            {
+                Command = command;
+            }
+            public Task Execute(CancellationToken cancellationToken) => Task.FromResult(Command);
+        }
+
+        public class Command : ICommand
+        {
+            public Command(int commandTag)
+            {
+                CommandTag = commandTag;
+            }
+
+            public int CommandTag { get; }
+        }
+
+        public class Installer : IWindsorInstaller
+        {
+            public HandlerFactorySelector HandlerFactorySelector { get; } = new HandlerFactorySelector();
+            private readonly FromDescriptor[] _searchHandlers;
+
+            public Installer(params FromDescriptor[] searchHandlers)
+            {
+                _searchHandlers = searchHandlers;
+            }
+
+            public void Install(IWindsorContainer container, IConfigurationStore store)
+            {
+                container.Register(
+                    Component.For<HandlerFactorySelector>().Instance(HandlerFactorySelector),
+                    Component.For<IHandlerFactory>().LifestyleSingleton()
+                        .AsFactory(c => c.SelectedWith<HandlerFactorySelector>())
+                );
+                if (_searchHandlers != null)
+                {
+                    container.Register(
+                        _searchHandlers.Select(fromDescriptor =>
+                                fromDescriptor.BasedOn<IHandler>()
+                                    .If(t =>
+                                    {
+                                        var a = t.GetCustomAttribute<CommandHandlerAttribute>();
+                                        if (a != null)
+                                            HandlerFactorySelector.Register(a.CommandTag, t);
+                                        return a != null;
+                                    })
+                                    .WithServiceAllInterfaces()
+                                    .WithServiceSelf()
+                                    .Configure(c => c.LifestyleTransient().IsFallback()))
+                                    .Cast<IRegistration>()
+                            .ToArray());
+                }
+            }
+        }
+
+        protected override IWindsorContainer CreateWindsorContainer()
+        {
+            return new WindsorContainer()
+                .AddFacility<TypedFactoryFacility>()
+                .Install(new Installer(Types.FromAssemblyContaining(GetType())));
+        }
+
+        public class Handler0CommandTests : HandlerByCommandTagTests
+        {
+            [CommandHandler(0)]
+            public class Handler0 : HandlerBase
+            {
+                public Handler0(ICommand command) : base(command)
+                {
+                }
+            }
+
+            [Test]
+            public void DispatchCommand0ToCommand0Handler()
+            {
+                var hf = WindsorContainer.Resolve<IHandlerFactory>();
+                var cmd = new Command(0);
+                var handler = hf.ForCommand(cmd);
+                handler
+                    .Should().BeOfType<Handler0>()
+                    .Which.Command.Should().BeSameAs(cmd);
+            }
+
+            [Test]
+            public void Command0HandlerRegisteredToIHandler()
+                => WindsorContainer.Resolve<IHandler>(new {command = new Command(0)})
+                    .Should().BeOfType<Handler0>();
+
+            [Test]
+            public void Command0HandlerRegisteredToHandler0()
+                => WindsorContainer.Resolve<Handler0>(new {command = new Command(0)})
+                    .Should().BeOfType<Handler0>();
+        }
+
+        public class Handler1CommandTests : HandlerByCommandTagTests
+        {
+
+
+            [CommandHandler(1)]
+            public class Handler1 : HandlerBase
+            {
+                public Handler1(ICommand command) : base(command)
+                {
+                }
+            }
+
+            [Test]
+            public void DispatchCommand1ToCommand1Handler()
+            {
+                var hf = WindsorContainer.Resolve<IHandlerFactory>();
+                var cmd = new Command(1);
+                var handler = hf.ForCommand(cmd);
+                handler
+                    .Should().BeOfType<Handler1>()
+                    .Which.Command.Should().BeSameAs(cmd);
+            }
+        }
+
+
+        public class Hander2CommandTests: HandlerByCommandTagTests
+        {
+            [Test]
+            public void DispatchCommand2ToNoHandler()
+            {
+                var hf = WindsorContainer.Resolve<IHandlerFactory>();
+                var cmd = new Command(2);
+                Action a = () => hf.ForCommand(cmd);
+                a.ShouldThrow<Exception>();
+            }
+        }
+    }
+}
---
+++ WindsorTests/HandlerDispatching/ByCommandType/HandlerByCommandTypeTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WindsorTests.HandlerDispatching.ByCommandType
+{
+    public interface IHandler
+    {
+        Task Execute(CancellationToken cancellationToken);
+    }
+    [SuppressMessage("ReSharper", "TypeParameterCanBeVariant", Justification = "Handler selection is done on exact command type")]
+    public interface IHandler<TCommand>: IHandler
+    {
+        TCommand Command { get; }
+    }
+
+    public interface IHandlerFactory
+    {
+        IHandler ForCommand(object command);
+    }
+
+    public class HandlerFactorySelector : DefaultTypedFactoryComponentSelector
+    {
+        public HandlerFactorySelector(
+            bool getMethodsResolveByName = true, 
+            bool fallbackToResolveByTypeIfNameNotFound = true) 
+            : base(getMethodsResolveByName, fallbackToResolveByTypeIfNameNotFound)
+        {
+        }
+
+        protected override Type GetComponentType(MethodInfo method, object[] arguments)
+        {
+            var command = arguments[0];
+            var componentType = typeof(IHandler<>).MakeGenericType(command.GetType());
+            return componentType;
+        }
+    }
+    public class HandlerBase<TCommand> : IHandler<TCommand>
+    {
+        public TCommand Command { get; }
+        public HandlerBase(TCommand command)
+        {
+            Command = command;
+        }
+        public Task Execute(CancellationToken cancellationToken) => Task.FromResult(Command);
+    }
+
+
+    public class HandlerByCommandTypeTests : AbstractWindsorContainerPerTest
+    {
+        protected override IWindsorContainer CreateWindsorContainer() => new WindsorContainer()
+            .AddFacility<TypedFactoryFacility>()
+            .Register(
+                Component.For<HandlerFactorySelector>().LifestyleSingleton(),
+                Component.For<IHandlerFactory>().LifestyleSingleton().AsFactory(c => c.SelectedWith<HandlerFactorySelector>()),
+                Types.FromAssemblyContaining(GetType())
+                    .BasedOn(typeof(IHandler<>))
+                    .WithService.FromInterface(typeof(IHandler<>))
+                    .Configure(c => c.LifestyleTransient().IsFallback())
+            );
+
+        public class Command0
+        {
+        }
+        public class Handler0 : HandlerBase<Command0>
+        {
+            public Handler0(Command0 command) : base(command)
+            {
+            }
+        }
+        [Test]
+        public void DispatchCommand0ToCommand0Handler()
+        {
+            var hf = WindsorContainer.Resolve<IHandlerFactory>();
+            var cmd = new Command0();
+            var handler = hf.ForCommand(cmd);
+            handler
+                .Should().BeOfType<Handler0>()
+                .Which.Command.Should().BeSameAs(cmd);
+        }
+
+
+        public class Command1
+        {
+        }
+
+        public class Handler1 : HandlerBase<Command1>
+        {
+            public Handler1(Command1 command) : base(command)
+            {
+            }
+        }
+        [Test]
+        public void DispatchCommand1ToCommand1Handler()
+        {
+            var hf = WindsorContainer.Resolve<IHandlerFactory>();
+            var cmd = new Command1();
+            var handler = hf.ForCommand(cmd);
+            handler
+                .Should().BeOfType<Handler1>()
+                .Which.Command.Should().BeSameAs(cmd);
+        }
+
+        public class Command2
+        {
+        }
+        [Test]
+        public void DispatchCommand2ToNoHandler()
+        {
+            var hf = WindsorContainer.Resolve<IHandlerFactory>();
+            var cmd = new Command2();
+            Action a = () => hf.ForCommand(cmd);
+            a.ShouldThrow<ComponentRegistrationException>();
+        }
+    }
+}
